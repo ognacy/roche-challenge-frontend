16ee7367e6423f5996e381fb1c3da572992d02a8
Tamalera <florence.meier@alumni.ethz.ch>
2021-09-25 13:56:03 +0200

    Add posts to start and reply scaffolding

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,4 +1,5 @@
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
+import axios from "axios";
 import {
   IonContent,
   IonRow,
@@ -11,9 +12,13 @@ import {
   IonIcon,
 } from "@ionic/react";
 
-import { send } from 'ionicons/icons';
+import { send } from "ionicons/icons";
+
+const baseURL = "https://roche-build-eynfci34ea-ez.a.run.app/api/v1";
 
 const currentUser = "Robert";
+const responseIdCounter = 0;
+
 const bot_messages = [
   { index: 0, user: "BOT", text: "Hello friend!" },
   { index: 1, user: "Robert", text: "Good morning!" },
@@ -21,11 +26,51 @@ const bot_messages = [
   { index: 3, user: "Robert", text: "I am fine." },
 ];
 
-function reply() {
-  alert('Hello!');
-}
-
 const Chat = () => {
+  const [input, setInput] = useState("");
+  const onAnswerChange = (e) => setInput(e.target.value);
+
+  useEffect(() => {
+    axios
+      .post(`${baseURL}/start`, {
+        user_id: "1",
+        resume: true,
+        questionnaire_type: "colon",
+      })
+      .then(() => {
+        console.log("POST to /start successful");
+      });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const answer = input;
+
+    console.log(answer);
+    if (answer) {
+      axios
+        .post(
+          `${baseURL}/reply`,
+          {
+            chatId: "1",
+            responseId: responseIdCounter + 1,
+            userResponse: answer,
+            suggestedResponseUsedId: null,
+          },
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods":
+                "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+            },
+          }
+        )
+        .then(() => {
+          console.log("POST to /reply successful");
+        });
+    }
+  };
+
   return (
     <IonPage>
       <IonContent
@@ -59,12 +104,22 @@ const Chat = () => {
             <IonToolbar>
               <IonRow align-items-center no-padding>
                 <IonCol size="10">
-                  <textarea autosize="true" maxrows="3" className="message-input"></textarea>
+                  <textarea
+                    autosize="true"
+                    maxrows="3"
+                    className="message-input"
+                    value={input}
+                    onChange={onAnswerChange}
+                  ></textarea>
                 </IonCol>
                 <IonCol size="2">
-                  <IonButton className="msg-btn" expand="block" fill="clear"
-                  onClick={reply}>
-                  <IonIcon icon={send}></IonIcon>
+                  <IonButton
+                    className="msg-btn"
+                    expand="block"
+                    fill="clear"
+                    onClick={handleSubmit}
+                  >
+                    <IonIcon icon={send}></IonIcon>
                   </IonButton>
                 </IonCol>
               </IonRow>
