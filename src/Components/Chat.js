959372b52f4ef201d14d6a5b028abad79447a626
Tamalera <florence.meier@alumni.ethz.ch>
2021-09-25 11:12:23 +0200

    Add dummy chat

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,12 +1,43 @@
-import { memo } from 'react';
-import { IonButton } from '@ionic/react';
+import { memo } from "react";
+import { IonContent, IonRow, IonCol, IonPage, IonGrid } from "@ionic/react";
+
+const currentUser = "Robert";
+const bot_messages = [
+  { index: 0, user: "BOT", text: "Hello friend!" },
+  { index: 1, user: "Robert", text: "Good morning!" },
+  { index: 2, user: "BOT", text: "How are you today?" },
+  { index: 3, user: "Robert", text: "I am fine." },
+];
 
 const Chat = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
-}
+    <IonPage>
+      <IonContent
+        scrollEvents={true}
+        onIonScrollStart={() => {}}
+        onIonScroll={() => {}}
+        onIonScrollEnd={() => {}}
+      >
+        <div>
+          <IonGrid>
+            <IonRow>
+              {bot_messages.map(
+                (msg) =>
+                  (
+                    <IonCol offset={currentUser === msg.user ? 3 : 0} size="9" key={msg.index} className={"message " + (currentUser === msg.user ? 'user-message': 'bot-message')}>
+                      <b>{msg.user}</b>
+                      <br />
+                      <span>{msg.text}</span>
+                      <br />
+                    </IonCol>
+                  )
+              )}
+            </IonRow>
+          </IonGrid>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default memo(Chat);
